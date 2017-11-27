309b82462f8a7ca01fa451955b49b6207a57a467
Mäder Michael <mike@moik.org>
2017-11-27 07:35:13 +0100

    creation of the SQLite DB

--- sonnenbatterie.py
+++ sonnenbatterie.py
@@ -1,10 +1,28 @@
 import requests
+import sqlite3
 
 def getSonnenData():
     r = requests.get('http://SB-41059:8080/api/v1/status')
     return r.json()
 
 def main():
+    conn = sqlite3.connect('sonnen.sql')
+    c = conn.cursor()
+
+    sql = """
+      CREATE TABLE battery
+     (consumption INTEGER, frequency INTEGER, gridConsumption INTEGER, isSystemInstalled INTEGER, pacTotal INTEGER,
+     production INTEGER, rsoc INTEGER, ts TEXT , usoc INTEGER, uAC INTEGER, uBat INTEGER)
+     """
+    # create table
+    c.execute(sql)
+    conn.commit()
+    conn.close()
+
+    exit(1)
+
+
+
     sonnenData = getSonnenData()
     print(sonnenData)
 
